386831e7c0b9f999963f3815a88d57e01791fb0c
Ting Pan <ting@Tings-MacBook-Pro.local>
2019-06-06 12:45:46 -0500

    Updates

---
+++ precache-manifest.90d4093dfc501f4fdbf6da3a5e7c9001.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2061063baa74bf60c8c3c6795f6813da",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "45e736b63addde6894e8",
+    "url": "/TodoList/static/css/main.762994d5.chunk.css"
+  },
+  {
+    "revision": "158ceb21e337d7808287",
+    "url": "/TodoList/static/js/2.2fb05ead.chunk.js"
+  },
+  {
+    "revision": "45e736b63addde6894e8",
+    "url": "/TodoList/static/js/main.8ba66e75.chunk.js"
+  },
+  {
+    "revision": "23ef1ee30c4a1e2cab36",
+    "url": "/TodoList/static/js/runtime~main.90b1048d.js"
+  }
+]);
