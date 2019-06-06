43129a8f0b8009e20261e1624eae8b6b1130e574
Ting Pan <ting@Tings-MacBook-Pro.local>
2019-06-06 12:35:10 -0500

    initial commit

---
+++ build/precache-manifest.db75c01c7809e5d8a463a7bc2e95f2aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e67c09f6f2e8eab67b2c6ca8066ac03c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8da69c00dc0834279b75",
+    "url": "/static/css/main.762994d5.chunk.css"
+  },
+  {
+    "revision": "158ceb21e337d7808287",
+    "url": "/static/js/2.2fb05ead.chunk.js"
+  },
+  {
+    "revision": "8da69c00dc0834279b75",
+    "url": "/static/js/main.1db838c1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
